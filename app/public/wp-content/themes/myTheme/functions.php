716915a7c28ad932de5ae7b1209a08dcbb266252
newcodesubed <subedshahxi@gmail.com>
2026-05-27 11:12:17 +0545

    feat: add support for post thumbnails and implement featured image display in single post template

--- app/public/wp-content/themes/myTheme/functions.php
+++ app/public/wp-content/themes/myTheme/functions.php
@@ -28,6 +28,7 @@ add_action('wp_enqueue_scripts', 'load_js');
 // Theme Options
 
     add_theme_support('menus');
+    add_theme_support('post-thumbnails');
 
 
 
@@ -38,3 +39,7 @@ register_nav_menus(
         'mobile-menu' => 'Mobile Menu Location'
     )
 );
+
+// custom image size
+add_image_size('blog-small', 300, 200, true);
+add_image_size('blog-large', 800, 400, true);
