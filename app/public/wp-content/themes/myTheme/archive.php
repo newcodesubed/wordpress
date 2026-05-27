dba2140c41f37b72101bda092e3098f47fa166a5
newcodesubed <subedshahxi@gmail.com>
2026-05-27 12:25:04 +0545

    feat: restructure layout for archive and page templates, add sidebar support, and enhance styling for better responsiveness

--- app/public/wp-content/themes/myTheme/archive.php
+++ app/public/wp-content/themes/myTheme/archive.php
@@ -1,7 +1,17 @@
 <?php get_header();?>
 <section class="page-wrap">
 <div class="container">
-    
+<section class="row">
+
+        <div class="col-lg-3">
+
+    <?php if(is_active_sidebar('blog-sidebar')):?>
+        <?php dynamic_sidebar('blog-sidebar'); ?>
+    <?php endif;?>
+        </div>
+
+        <div class="col-lg-9">
+
     <h1><?php echo single_cat_title(); ?></h1>
 
     <?php get_template_part('includes/section', 'archive'); ?>
@@ -13,8 +23,10 @@
     'format' => '?paged=%#%',
     'current' => max( 1, get_query_var('paged') ),
     'total' => $wp_query->max_num_pages
-) );
-?>
+    ) );
+    ?>
+    </div>
+    </section>    
 </div>
 </section>
 <?php get_footer();?>
--- app/public/wp-content/themes/myTheme/functions.php
+++ app/public/wp-content/themes/myTheme/functions.php
@@ -29,6 +29,7 @@ add_action('wp_enqueue_scripts', 'load_js');
 
     add_theme_support('menus');
     add_theme_support('post-thumbnails');
+    add_theme_support('widgets');
 
 
 
@@ -43,3 +44,27 @@ register_nav_menus(
 // custom image size
 add_image_size('blog-small', 300, 200, true);
 add_image_size('blog-large', 800, 400, true);
+
+// sidebar
+
+function my_sidebar(){
+    register_sidebar(
+        array(
+            'name' => 'Page Sidebar',
+            'id' => 'page-sidebar',
+            'before_widget' => '<h4 class="widget-title">',
+            'after_widget' => "</h4>",
+            
+        )
+    );
+    register_sidebar(
+        array(
+            'name' => 'Blog Sidebar',
+            'id' => 'blog-sidebar',
+            'before_widget' => '<h4 class="widget-title">',
+            'after_widget' => "</h4>",
+            
+        )
+    );
+}
+add_action('widgets_init', 'my_sidebar');
