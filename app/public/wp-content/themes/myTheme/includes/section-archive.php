6f4f18b2b32d03edb3d425ba378c0b289076f948
newcodesubed <subedshahxi@gmail.com>
2026-05-27 11:31:26 +0545

    feat: enhance archive and page templates with improved layout and featured image handling

--- app/public/wp-content/themes/myTheme/includes/section-archive.php
+++ app/public/wp-content/themes/myTheme/includes/section-archive.php
@@ -1,12 +1,19 @@
 <?php if(have_posts()): while(have_posts()): the_post();?>
 <div class="card">
-    <div class="card-body mb-3">
+    <div class="card-body d-flex justify-content-center align-items-center">
+        
+        <?php if(has_post_thumbnail()):?>
+        <img src="<?php the_post_thumbnail_url('blog-small'); ?>" alt="<?php the_title(); ?>" class="image-fluid mb-3 img-thumbnail me-4">
+        <?php endif;?>
 
-        <h3>
-            <?php the_title();?>
-        </h3> 
-        <?php the_excerpt();?>
-        <a href="<?php the_permalink();?>" class='btn btn-success'>Read More</a>
+        <div class="blog-content">
+
+            <h3>
+                <?php the_title();?>
+            </h3> 
+            <?php the_excerpt();?>
+            <a href="<?php the_permalink();?>" class='btn btn-success'>Read More</a>
+        </div>
     </div>
 </div>
 <?php endwhile; else: endif;?>
--- app/public/wp-content/themes/myTheme/page.php
+++ app/public/wp-content/themes/myTheme/page.php
@@ -1,6 +1,9 @@
 <?php get_header();?>
 <section class="page-wrap">
 <div class="container">
+    <?php if(has_post_thumbnail()):?>
+        <img src="<?php the_post_thumbnail_url('blog-large'); ?>" alt="<?php the_title(); ?>" class="image-fluid mb-3 img-thumbnail">
+        <?php endif;?>
     
     <h1><?php the_title(); ?></h1>
 
